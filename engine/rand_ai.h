3e59cb59848d81bb936169fec096af36dfc83389
pacificbelt30 <pacificbelt0201.outlook.jp>
2021-03-25 01:53:50 +0900

    wslとwindowsのファイルのやり取りがもう少し楽になればいいのに

--- engine/rand_ai.h
+++ engine/rand_ai.h
@@ -1,7 +1,13 @@
 #include<stdint.h>
+#include "../engine.h"
 
 #ifndef _RANDAI_
 #define _RANDAI_
 void randAi();//rand着手のAI
 uint64_t randPos(uint64_t legalboard);//合法手から一手選ぶ
+class RandAIEngine : public Engine{
+  public:
+  private:
+    uint64_t randPos(uint64_t legalboard);
+};
 #endif

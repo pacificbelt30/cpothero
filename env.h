64fb3988563dc93b2f0b1ea6d1e66bb0e8488e23
pacificbelt30 <pacificbelt0201.outlook.jp>
2021-04-11 00:31:52 +0900

    今期の私は無理です

--- env.h
+++ env.h
@@ -2,8 +2,8 @@
 #include "./engine/rand_ai.h"
 #ifndef __ENV__
 #define __ENV__
-//const bool DEBUG_MODE = false;
-const bool DEBUG_MODE = true;
+const bool DEBUG_MODE = false;
+//const bool DEBUG_MODE = true;
 //using ENGINE = RandAIEngine;
 using ENGINE = One;
 #endif
